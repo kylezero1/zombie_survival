97c571706f649ff3fc59594745c8171139bf2f45
kylezero1 <kylezero1@gmail.com>
2020-07-13 21:51:39 -0700

    Added character gun shooting animation

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,30 +7,46 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
-
+    private Animator animator;
     private AudioSource audioSource;
 
     public bool canShoot;
+    private bool timerOn = false;
+    private float timer = 0.6f;
 
     public float bulletForce = 20f;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         canShoot = true;
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1") && canShoot)
-        Shoot();        
+        if (Input.GetButtonDown("Fire1") && canShoot && timer >= .6f)
+        {
+            Shoot();
+        }
+        if (timerOn)
+        {
+            timer += Time.deltaTime;
+            if (timer >= .6f)
+            {
+                timerOn = false;
+            }
+        }
     }
 
     void Shoot()
     {
         audioSource.Play();
+        animator.SetTrigger("gunShot");
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        timerOn = true;
+        timer = 0.0f;
     }
 }
